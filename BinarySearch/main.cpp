f218c5695386d1d54e5570ca16ac97084a94ff96
Cong <Cong@CongmatoiMac.local>
2013-03-31 19:23:28 +0800

    BinarySearch

--- BinarySearch/main.cpp
+++ BinarySearch/main.cpp
@@ -7,12 +7,30 @@
 //
 
 #include <iostream>
+using namespace std;
+
+int BinarySearch(int data[], int start, int end, int k){
+    while(start <= end){
+        int mid = start + (end - start) / 2;
+        //cout<<mid<<endl;
+        if(data[mid] == k)
+            return mid;
+        else if(data[mid] < k){
+            start = mid + 1;
+        }else{
+            end = mid - 1;
+        }
+    }
+    //if(start > end)
+        return -1;
+}
+
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    cout<<BinarySearch(data, 0, 9, 0)<<endl;
+    
     return 0;
 }
 
